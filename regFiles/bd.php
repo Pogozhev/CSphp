1d9180921b9ee0f8801e8d16a7744d849d739651
Nikita Aplin <aplinxy9plin@icloud.com>
2017-11-30 13:22:58 +0700

    Change DB

--- regFiles/bd.php
+++ regFiles/bd.php
@@ -1,4 +1,4 @@
 <?php
-$mysqli = new mysqli("localhost","top4ek","q2w3e4r5","cropsafe");
+$mysqli = new mysqli("localhost","n99949ql_phpshop","q2w3e4r5","n99949ql_phpshop");
 //$mysqli->set_charset("utf8");
 ?>
