d1a350252efb540fc16488a8490c5520e208011e
Nikita Aplin <aplinxy9plin@icloud.com>
2017-11-30 13:23:22 +0700

    EZ PZ

--- regFiles/bd.php
+++ regFiles/bd.php
@@ -1,4 +1,4 @@
 <?php
-$mysqli = new mysqli("localhost","n99949ql_phpshop","q2w3e4r5","n99949ql_phpshop");
+$mysqli = new mysqli("localhost","n99949ql_phpshop","562798","n99949ql_phpshop");
 //$mysqli->set_charset("utf8");
 ?>
